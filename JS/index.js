c4208dcbc1b9fb163476bacbd2c4a127ecb045ee
Siarhei Kharlanau1 <siarhei_kharlanau1@epam.com>
2021-10-30 14:57:08 +0300

    refactor: refactor index file

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,13 @@
-const Plane = require('./Planes/Plane');
-const MilitaryPlane = require('./Planes/MilitaryPlane');
-const PassengerPlane = require('./Planes/PassengerPlane');
-const Airport = require('./Airport');
 const MilitaryType = require('./models/MilitaryType');
-const experimentalPlane = require('./Planes/experimentalPlane');
-const ExperimentalTypes = require('./models/ExperimentalTypes');
+const ExperimentalTypes = require('./models/experimentalTypes');
 const ClassificationLevel = require('./models/ClassificationLevel');
+const MilitaryPlane = require('./planes/MilitaryPlane');
+const PassengerPlane = require('./planes/PassengerPlane');
+const ExperimentalPlane = require('./planes/ExperimentalPlane');
+const Airport = require('./Airport');
 
 (function run() {
-
-    let planes = [
+    const planes = [
         new PassengerPlane('Boeing-737', 900, 12000, 60500, 164),
         new PassengerPlane('Boeing-737-800', 940, 12300, 63870, 192),
         new PassengerPlane('Boeing-747', 980, 16100, 70500, 242),
@@ -23,15 +21,15 @@ const ClassificationLevel = require('./models/ClassificationLevel');
         new MilitaryPlane('B-52 Stratofortress', 1000, 20000, 80000, MilitaryType.TYPE_BOMBER),
         new MilitaryPlane('F-15', 1500, 12000, 10000, MilitaryType.TYPE_FIGHTER),
         new MilitaryPlane('F-22', 1550, 13000, 11000, MilitaryType.TYPE_FIGHTER),
-        new MilitaryPlane('C-130 Hercules', 650, 5000, 110000, MilitaryType.TRANSPORT),
-        new experimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
-        new experimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
+        new MilitaryPlane('C-130 Hercules', 650, 5000, 110000, MilitaryType.TYPE_TRANSPORT),
+        new ExperimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
+        new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
     ];
 
-    let airport = new Airport(planes);
-    let militaryAirport = new Airport(airport.getMilitaryPlanes());
-    let passengerAirport = new Airport(airport.getPasPl());
-    console.log(`Military airport sorted by max distance: ${Airport.print(militaryAirport.sortByMaxDistance())}`);
-    console.log(`Passenger airport sorted by max speed: ${Airport.print(passengerAirport.sortByMaxSpeed())}`);
-    console.log(`Plane with max passenger capacity: ${Airport.print(passengerAirport.getPassengerPlaneWithMaxPassengersCapacity())}`);
+    const airport = new Airport(planes);
+    const militaryAirport = new Airport(airport.getMilitaryPlanes());
+    const passengerAirport = new Airport(airport.getPassengerPlanes());
+    console.log(`Military airport sorted by max distance: ${Airport.printPlanes(militaryAirport.sortByMaxDistance())}`);
+    console.log(`Passenger airport sorted by max speed: ${Airport.printPlanes(passengerAirport.sortByMaxSpeed())}`);
+    console.log(`Plane with max passenger capacity: ${Airport.printPlanes(passengerAirport.getPassengerPlaneWithMaxPassengersCapacity())}`);
 })();
